1b5b8a69ed44585e4c090d226c4c49fd6806f595
aqsababar5566 <f190979@cfd.nu.edu.pk>
2022-12-09 03:11:51 +0500

    Updating the insertion code for inserting mutants in database

--- src/DatabaseAccess/Data_Insertor.java
+++ src/DatabaseAccess/Data_Insertor.java
@@ -9,76 +9,69 @@ import java.util.LinkedList;
 
 import javax.swing.JOptionPane;
 
-
-
 import BussinessLogic.makeMutant;
 
 public class Data_Insertor {
-	
-	LinkedList Key = new LinkedList();
 
-public LinkedList GetData() {
-		  
-		
-	    LinkedList word1 = new LinkedList();
-		
-		
+	LinkedList getValue = new LinkedList();
+
+	public LinkedList GetData() {
+
+		LinkedList word1 = new LinkedList();
+
 		try {
-	    	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker","root","");
-	    	
-	    	if (!con.isClosed())
-	    	   {
-	    		PreparedStatement ps = con.prepareStatement("SELECT * from word");
-	          
-	            ResultSet rs = ps.executeQuery();
-	    	      
-	    	      while (rs.next())
-	    	      {
-	    	    	 String arr;
-	    	        arr=(String) rs.getObject("words");
-	    	        Key.add((Integer)rs.getObject("word_id"));
-	    	        word1.add(arr);
-	    	        
-	    	      }
-	    	     
-	    	      con.close();
-	    	   }
-	    	   else
-	    	      System.out.println("System fail");
-	    	}
-	    catch(SQLException e) {
-	    	System.out.println(e);
-	    }
-          return(word1);
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker", "root", "");
+
+			if (!con.isClosed()) {
+				PreparedStatement ps = con.prepareStatement("SELECT * from word");
+
+				ResultSet rs = ps.executeQuery();
+
+				while (rs.next()) {
+					String arr;
+					arr = (String) rs.getObject("words");
+					getValue.add((Integer) rs.getObject("word_id"));
+					word1.add(arr);
+
+				}
+
+				con.close();
+			} else
+				System.out.println("System fail");
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		return (word1);
 	}
 
-public void Insert() {
-	makeMutant m1=new makeMutant();
-	
-	 LinkedList L1=new LinkedList();
-	 LinkedList L2=new LinkedList();
-	 L1=m1.Key();
-	 L2=m1.Mut();
-	  
-try { 
-	  Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker","root","");
-	  for(int i=0;i<L2.size();i++) { 
-		  
-		  String Word=(String) L2.get(i);
-		  int key=(int) L1.get(i);
-		  
-      PreparedStatement st = con.prepareStatement("insert into Mutants values("+null+",'"+Word+"',"+key+")");
-      st.execute();
-     
-} 
-	  con.close(); 
-JOptionPane.showMessageDialog(null,"Mutants Inserted Succesfully into Database"); 
-} 
-catch(SQLException e) 
-{ System.out.println(e); 
-}
-}
-public LinkedList ReturnKey() {
-	return Key;
-}
+	public void Insert() {
+		makeMutant setMutant = new makeMutant();
+
+		LinkedList L1 = new LinkedList();
+		LinkedList L2 = new LinkedList();
+		L1 = setMutant.Key();
+		L2 = setMutant.Mut();
+
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker", "root", "");
+			for (int i = 0; i < L2.size(); i++) {
+
+				String Word = (String) L2.get(i);
+				int key = (int) L1.get(i);
+
+				PreparedStatement st = con
+						.prepareStatement("insert into Mutants values(" + null + ",'" + Word + "'," + key + ")");
+				st.execute();
+
+			}
+			con.close();
+			JOptionPane.showMessageDialog(null, "Mutants inserted in DB");
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+
+	public LinkedList ReturnKey() {
+		return getValue;
+	}
 }
