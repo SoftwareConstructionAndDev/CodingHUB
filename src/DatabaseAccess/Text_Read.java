36fd8a963ecd40317db812866b75cf8462060006
aqsababar5566 <f190979@cfd.nu.edu.pk>
2022-12-09 03:17:57 +0500

    Fetching words from database for usecase 2

--- src/DatabaseAccess/Text_Read.java
+++ src/DatabaseAccess/Text_Read.java
@@ -1,4 +1,5 @@
 package DatabaseAccess;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,47 +11,112 @@ public class Text_Read {
 	static String Title;
 	static String Author;
 	static String Paragraph;
-	
-	
+	int id;
+
 	public static ArrayList<String> errorlist = new ArrayList<String>();
-	
-	
-public void connection_FUN(String word) {
-  
-	
-	try {
-    	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker","root","");
-    	
-    	if (!con.isClosed())
-    	   {
-    		PreparedStatement ps = con.prepareStatement("SELECT * FROM word WHERE words LIKE '" + word + "'");
-          
-            ResultSet rs = ps.executeQuery();
-    	      
-    	      if (rs.next()) {
-    	    	 
-    	    	  
-                 
-              } else {
-            	 
-            	  errorlist.add(word);
-            	  
-              }
+	public static ArrayList<String> errorlist2 = new ArrayList<String>();
 
-    	      con.close();
-    	   }
-    	   else
-    	      System.out.println("Connect fail");
-    	}
-    catch(SQLException e) {
-    	System.out.println(e);
-    }
+	public void connection_FUN(ArrayList words) {
+		errorlist.clear();
+
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker", "root", "");
+
+			for (int i = 0; i < words.size(); i++) {
+				PreparedStatement ps = con
+						.prepareStatement("SELECT * FROM word WHERE words LIKE '" + words.get(i) + "'");
+
+				ResultSet rs = ps.executeQuery();
+
+				if (rs.next()) {
+
+				} else {
+
+					errorlist.add((String) words.get(i));
+
+				}
+			}
+			con.close();
+
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+
+	}
+
+	public void connection(ArrayList words) {
+		errorlist2.clear();
+
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker", "root", "");
+
+			for (int i = 0; i < words.size(); i++) {
+				PreparedStatement ps = con
+						.prepareStatement("SELECT * FROM mutants WHERE Word LIKE '" + words.get(i) + "'");
+
+				ResultSet rs = ps.executeQuery();
+
+				if (rs.next()) {
+
+					errorlist2.add((String) words.get(i));
+
+				} else {
+
+				}
+			}
+			con.close();
+
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+
+	}
+
+	public String connection3(String word) {
+
+		int id = 0;
+		String Nword = null;
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spell_checker", "root", "");
+
+			PreparedStatement ps = con.prepareStatement("SELECT * FROM mutants WHERE Word LIKE '" + word + "'");
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				id = (int) rs.getObject("F_Key");
+
+			} else {
+
+			}
+
+			PreparedStatement ps1 = con.prepareStatement("SELECT * FROM word WHERE word_id = '" + id + "'");
+
+			ResultSet rs1 = ps1.executeQuery();
+
+			if (rs1.next()) {
+
+				Nword = (String) rs1.getObject("words");
+
+			}
+
+			con.close();
+
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+
+		return Nword;
+	}
+
+	public ArrayList<String> ErrorList() {
+
+		return errorlist;
+	}
+
+	public ArrayList<String> ErrorList2() {
+
+		return errorlist2;
+	}
 
 }
- public ArrayList<String> ErrorList() {
-	 
-	 return errorlist;
- }
-
-}
-
