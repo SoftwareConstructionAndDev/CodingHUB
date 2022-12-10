285f09c999056d72eb94a47dd27e7f6084da4150
unknown <f200424@cfd.nu.edu.pk>
2022-12-10 16:38:19 +0500

    View for Mutant Generator

--- src/Presentation/Mutants_Generator.java
+++ src/Presentation/Mutants_Generator.java
@@ -1,19 +1,18 @@
 package Presentation;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import BussinessLogic.makeMutant;
-
-import javax.swing.JLabel;
-import java.awt.Font;
-import java.awt.Color;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class Mutants_Generator extends JFrame {
 
